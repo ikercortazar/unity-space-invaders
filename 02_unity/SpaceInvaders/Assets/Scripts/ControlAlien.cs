4c6d4546910c97c8427fba31c2eae7a85b99e5df
ikercortazar <ikermanjava@gmail.com>
2016-05-12 21:56:12 +0200

    Bajando toda la columna.
    Se consigue que baje por columnas.

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -5,24 +5,72 @@ public class ControlAlien : MonoBehaviour
 {
 	// Conexión al marcador, para poder actualizarlo
 	public GameObject marcador;
+    int sentido = 0;
+    int contador = 0;
+    private float velocidad = 2f;
+   
 
-	// Por defecto, 100 puntos por cada alien
-	public int puntos = 100;
+    // Por defecto, 100 puntos por cada alien
+    public int puntos = 100;
 
 	// Use this for initialization
 	void Start ()
 	{
-		// Localizamos el objeto que contiene el marcador
-		marcador = GameObject.Find ("Marcador");
+        // Localizamos el objeto que contiene el marcador
+        marcador = GameObject.Find ("Marcador");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
-	}
+        // Calculamos la anchura visible de la cámara en pantalla
 
-	void OnCollisionEnter2D (Collision2D coll)
+        float distanciaHorizontal = Camera.main.orthographicSize * Screen.width / Screen.height;
+        //int sentido = 0;
+
+    // Calculamos el límite izquierdo y el derecho de la pantalla
+    float limiteIzq = -1.0f * distanciaHorizontal;
+        float limiteDer = 1.0f * distanciaHorizontal;
+
+        // Tecla: Izquierda
+        if (sentido == 0)
+        {
+
+            // Nos movemos a la izquierda hasta llegar al límite para entrar por el otro lado
+            if (transform.position.x > limiteIzq)
+            {
+                transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+            }
+            else {
+                contador++;
+                if (contador == 7)
+                {
+                    transform.position = new Vector2(limiteIzq, transform.position.y - 4);
+                    transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+                    contador = 0;
+                    sentido = 1;
+               }
+            }
+        }
+        else {
+            if (transform.position.x < limiteDer)
+            {
+                transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+            }
+            else {
+                contador++;
+                if (contador == 7)
+                {
+                    transform.position = new Vector2(limiteDer, transform.position.y - 4);
+                    transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+                    contador = 0;
+                    sentido = 0;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D coll)
 	{
 		// Detectar la colisión entre el alien y otros elementos
 
